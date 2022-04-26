722a9d82063863a777f0c5ae9743e1ddc4715839
MohdKhalid13 <mohdkhalid.mansoori@gmail.com>
2022-04-26 15:45:43 +0530

    Document Object Model

---
+++ dom.js
@@ -0,0 +1,9 @@
+function hello()
+{
+    document.getElementById('demo').innerHTML  =  document.getElementById('naam').value;
+}
+
+function output()
+{
+    document.getElementById('Hera').value = document.getElementById('demo').innerHTML
+}
